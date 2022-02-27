88a54a3e7e372eb0ec27280b6ed3052c9ba3e838
Kate Higa <khiga8@github.com>
2022-02-27 14:34:10 -0800

    change codepen to correct case

--- src/components/social-links.js
+++ src/components/social-links.js
@@ -10,7 +10,7 @@ const SocialLinks = () => {
           <li style={{marginRight: rhythm(1), display: "inline"}}><a target='_blank' rel='noreferrer' href='https://dev.to/kateh'>DEV</a></li>
           <li style={{marginRight: rhythm(1), display: "inline"}}><a target='_blank' rel='noreferrer' href='https://twitter.com/kate8keito'>Twitter</a></li>
           <li style={{marginRight: rhythm(1), display: "inline"}}><a target='_blank' rel='noreferrer' href='https://www.linkedin.com/in/kate-higa-2bab87108/'>LinkedIn</a></li>
-          <li style={{marginRight: rhythm(1), display: "inline"}}><a target='_blank' rel='noreferrer' href='https://codepen.io/khiga8'>Codepen</a></li>
+          <li style={{marginRight: rhythm(1), display: "inline"}}><a target='_blank' rel='noreferrer' href='https://codepen.io/khiga8'>CodePen</a></li>
           <li style={{marginRight: rhythm(1), display: "inline"}}><a target='_blank' rel='noreferrer' href='https://github.com/khiga8'>GitHub</a></li>
       </ul>
     </nav>
